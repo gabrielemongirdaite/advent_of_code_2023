82d4301f95946ac1324f90d7c397b11486a7b095
gabrielemongirdaite <gabriele.mongirdaite@gmail.com>
2023-12-14 09:08:12 +0200

    day 12: both parts

--- day12.py
+++ day12.py
@@ -1,5 +1,7 @@
+import copy
 import time
 import itertools
+from collections import Counter
 
 
 def read_file(file_name):
@@ -45,27 +47,6 @@ def count_combinations(spring, group):
     return r, possible_springs
 
 
-def replacing_question_marks(possible_combinations):
-    new_spring = ''
-    for i in list(zip(*possible_combinations)):
-        if len(set(i)) == 1:
-            new_spring += i[0]
-        else:
-            new_spring += '?'
-    return new_spring
-
-
-# def possibilities(possible_combinations):
-#     poss = []
-#     all_indices = []
-#     for i in possible_combinations:
-#         current_setup, current_indices = current_grouping(i, '#')
-#         all_indices.append(current_indices)
-#     for i in list(zip(*all_indices)):
-#         poss.append(len(set(i)))
-#     return poss
-
-
 def arrangements(spring, group):
     if '?' not in spring:
         return 1
@@ -73,62 +54,94 @@ def arrangements(spring, group):
         return count_combinations(spring, group)[0]
 
 
+def remove_impossible_second_try(possibility, group, spring):
+    if possibility[1] == 0 and possibility[2] != group[0:len(possibility[2])]:
+        return False
+    elif possibility[1] == 1 and possibility[2][-1] > group[0:len(possibility[2])][-1]:
+        return False
+    elif possibility[0] == len(spring) - 1 and possibility[2] != group:
+        return False
+    else:
+        return True
+
+
+def update_groups(in_group, current_group):
+    current_group_copy = copy.deepcopy(current_group)
+    if in_group == 0:
+        current_group_copy.append(1)
+    else:
+        current_group_copy[-1] += 1
+    return current_group_copy
+
+
+def all_possibilities_second_try(spring, group):
+    possibilities = []
+    if spring[0] == '?':
+        possibilities.append([0, 0, [], 1])
+        possibilities.append([0, 1, [1], 1])
+    else:
+        possibilities.append([0, 1 if spring[0] == '#' else 0, [1] if spring[0] == '#' else [], 1])
+    for ind, i in enumerate(spring[1:]):
+        new_possibilities = []
+        for k in possibilities:
+            if i == '?':
+                new_possibilities.append([ind + 1, 0, k[2], k[3]])
+                new_possibilities.append([ind + 1, 1, update_groups(k[1], k[2]), k[3]])
+            else:
+                new_possibilities.append(
+                    [ind + 1, 1 if i == '#' else 0, k[2] if i == '.' else update_groups(k[1], k[2]), k[3]])
+        possibilities = new_possibilities
+        poss_boolean = []
+        for j in possibilities:
+            poss_boolean.append(remove_impossible_second_try(j, group, spring))
+        possibilities = [x for x, y in zip(possibilities, poss_boolean) if y]
+        all_poss = dict(Counter(tuple(((i[0]),) + ((i[1]),) + ((tuple(i[2]),)) for i in possibilities)))
+        for j in all_poss:
+            r = 0
+            for t in possibilities:
+                if t[0] == j[0] and t[1] == j[1] and t[2] == list(j[2]):
+                    r += t[3]
+            all_poss[j] = r
+
+        possibilities = []
+        for j in all_poss:
+            possibilities.append([j[0], j[1], list(j[2]), all_poss[j]])
+    return sum([x[3] for x in possibilities])
+
+
 springs, groups = read_file('input_day12.txt')
 
-# start_time = time.time()
-# result = 0
-# for ind, i in enumerate(springs):
-#     result += arrangements(i, groups[ind])
-#
-# print(result)
-#
-# print("--- %s seconds for 1st part---" % (time.time() - start_time))
-
-new_springs = []
-
-for ind, i in enumerate(springs):
-    new_springs.append(replacing_question_marks(count_combinations(i, groups[ind])[1]))
-
-# ending_in_question = 0
-# for ind, i in enumerate(new_springs):
-#     print(i, groups[ind])
-#     ending_in_question += 1 if i[-1] == '?' else 0
-
+start_time = time.time()
 result = 0
 for ind, i in enumerate(springs):
-    if i[-1] == '#':
-        i = new_springs[ind]
-    r1 = arrangements(i, groups[ind])
-    r2 = arrangements('?' + i, groups[ind])
-    r3 = arrangements(i + '?', groups[ind])
-    if r1 * r2 ** 4 >= r3 ** 4 * r1:
-        result += r1 * r2 ** 4
-        #print(r1 * r2 ** 4)
-    else:
-        result += r3 ** 4 * r1
-        #print(r3 ** 4 * r1)
+    result += arrangements(i, groups[ind])
 
 print(result)
 
-# new_springs_part2 = []
-# groups_part2 = []
-#
-# for ind, i in enumerate(new_springs):
-#     if '?' in i:
-#         new_springs_part2.append(((i + '?') * 5)[:-1])
-#         groups_part2.append(groups[ind] * 5)
-#     else:
-#         new_springs_part2.append(i)
-#         groups_part2.append(groups[ind])
+print("--- %s seconds for 1st part (brute force)---" % (time.time() - start_time))
 
 
-# start_time = time.time()
-# result = 0
-# for ind, i in enumerate(new_springs_part2):
-#     print(ind, i)
-#     result += arrangements(i, groups_part2[ind])
+start_time = time.time()
+
+result = 0
+for ind, i in enumerate(springs):
+    result += all_possibilities_second_try(i, groups[ind])
+
+print(result)
+print("--- %s seconds for 1st part---" % (time.time() - start_time))
 
 
-# print(result)
-#
-# print("--- %s seconds for 1st part---" % (time.time() - start_time))
+start_time = time.time()
+new_springs_part2 = []
+groups_part2 = []
+
+for ind, i in enumerate(springs):
+    new_springs_part2.append(((i + '?') * 5)[:-1])
+    groups_part2.append(groups[ind] * 5)
+
+result = 0
+for ind, i in enumerate(new_springs_part2):
+    r = all_possibilities_second_try(i, groups_part2[ind])
+    result += r
+print(result)
+print("--- %s seconds for 2nd part---" % (time.time() - start_time))
